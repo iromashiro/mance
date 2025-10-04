11097e8b138b04aa9f2ea8ec7ac443df0ff4d1c8
iromashiro <bayughara8@gmail.com>
2025-10-04 21:42:20 +0700

    add trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
